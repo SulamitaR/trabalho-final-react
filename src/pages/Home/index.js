2e2822cecbd17c7046aa7b56509aa1c3f80fc583
SulamitaR <111604686+SulamitaR@users.noreply.github.com>
2022-11-05 16:14:43 -0300

    :sparkles: Criacao da pagina indique e criar contato

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ const Home= () => {
     return (
         <center>
               <h1>SEJA BEM VINDO A SERRA IMOVEIS</h1>
-              <h2>Aqui você fica por dentro das melhores opções de imovéis disponíveis na região serrana do estado fo Rio de Janeiro.</h2>
+              <h3>Aqui você fica por dentro das melhores opções de imovéis disponíveis na região serrana do estado do Rio de Janeiro.</h3>
         </center>
     )
 }
