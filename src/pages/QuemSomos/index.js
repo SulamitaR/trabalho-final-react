de7adcf4e3867f9db9c0b60b3db8cb843f8e0dcb
SulamitaR <111604686+SulamitaR@users.noreply.github.com>
2022-11-08 10:16:45 -0300

    Subindo quem somos(tarefa realizada pelo Gabriel Mendes)

--- src/pages/QuemSomos/index.js
+++ src/pages/QuemSomos/index.js
@@ -6,12 +6,14 @@ const QuemSomos = () => {
             <h1>Quem somos </h1>
 
             <body>
-                <p>O Serratec – Parque Tecnológico da Região Serrana é uma instituição privada, sem fins lucrativos e de interesse público. Principal indutor de negócios em TI do interior do estado do Rio de Janeiro, o
-                    Serratec é também o maior Parque Tecnológico do interior fluminense, abrangendo as cidades de Petrópolis, Teresópolis, Nova Friburgo e Areal. E Nele tem o melhor site de compra/venda e para alugueis de imóveis da
+                <p>O Serratec – Parque Tecnológico da Região Serrana é uma instituição privada, sem fins lucrativos e de interesse público.</p>
+                <p> Principal indutor de negócios em TI do interior do estado do Rio de Janeiro, o
+                    Serratec é também o maior Parque Tecnológico do interior fluminense, abrangendo as cidades de Petrópolis, Teresópolis, Nova Friburgo e Areal.</p>
+                <p> E Nele tem o melhor site de compra/venda e para alugueis de imóveis da
                     da Região serrana</p>
 
-                {/* Gabriel Mendes Card */}
-                
+                {}
+
                 <div className="col-12 col-md-6 col-lg-4 card-3">
                     <div className="card text-center">
                         <div className="card-body">
@@ -21,28 +23,92 @@ const QuemSomos = () => {
                             <p className="card-text">
                                 Teresópolis - RJ
                             </p>
-            
+
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div className="social">
-                <a href="https://pt-br.facebook.com/serratecoficial/" target="_blank"><i
-                    className="bi bi-facebook"></i></a>
-                <a href="https://www.instagram.com/serratecoficial/" target="_blank"><i
-                    className="bi bi-instagram"></i></a>
-                <a href="https://www.linkedin.com/company/serratecoficial/" target="_blank"><i
-                    className="bi bi-linkedin"></i></a>
-                <a href="https://www.youtube.com/channel/UC2pvxl7H3IK2X6z9XZ3xsyQ" target="_blank"><i
-                    className="bi bi-youtube"></i></a>
-                <a href="https://github.com/Material-Didatico-Serratec" target="_blank"><i
-                    className="bi bi-github"></i></a>
-            </div>
 
 
+                {/* Richard Cardinot Card */}
 
-        </body>
-            
+                <div className="col-12 col-md-6 col-lg-4 card-3">
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <img src="https://avatars.githubusercontent.com/u/73504375?v=4" alt="foto de perfil"
+                                className="img-fluid rounded-circle" />
+                            <h3 className="card-title py-2" >Richard Cardinot</h3>
+                            <p className="card-text">
+                                Teresópolis - RJ
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Márcia Lima Card */}
+
+                <div className="col-12 col-md-6 col-lg-4 card-3">
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <img src="" alt="foto de perfil"
+                                className="img-fluid rounded-circle" />
+                            <h3 className="card-title py-2" >Marcia Lima</h3>
+                            <p className="card-text">
+                                Nova Friburgo - RJ
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Gabriel Pacheco Card */}
+
+                <div className="col-12 col-md-6 col-lg-4 card-3">
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <img src="https://avatars.githubusercontent.com/u/112281707?v=4" alt="foto de perfil"
+                                className="img-fluid rounded-circle" />
+                            <h3 className="card-title py-2" >Gabriel Pacheco</h3>
+                            <p className="card-text">
+                                Nova Friburgo - RJ
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Gabriel Mendes Card */}
+
+                <div className="col-12 col-md-6 col-lg-4 card-3">
+                    <div className="card text-center">
+                        <div className="card-body">
+                            <img src="https://avatars.githubusercontent.com/u/98937422?v=4" alt="foto de perfil"
+                                className="img-fluid rounded-circle" />
+                            <h3 className="card-title py-2" >Gabriel Mendes</h3>
+                            <p className="card-text">
+                                Teresópolis - RJ
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className="social">
+                    <a href="https://pt-br.facebook.com/serratecoficial/" target="_blank"><i
+                        className="bi bi-facebook"></i></a>
+                    <a href="https://www.instagram.com/serratecoficial/" target="_blank"><i
+                        className="bi bi-instagram"></i></a>
+                    <a href="https://www.linkedin.com/company/serratecoficial/" target="_blank"><i
+                        className="bi bi-linkedin"></i></a>
+                    <a href="https://www.youtube.com/channel/UC2pvxl7H3IK2X6z9XZ3xsyQ" target="_blank"><i
+                        className="bi bi-youtube"></i></a>
+                    <a href="https://github.com/Material-Didatico-Serratec" target="_blank"><i
+                        className="bi bi-github"></i></a>
+                </div>
+
+
+
+            </body>
+
         </center >
     )
 }
