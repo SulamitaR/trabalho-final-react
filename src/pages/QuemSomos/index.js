2dc98d48614f4d32a585ec6b798abc48ec8b9bc8
SulamitaR <111604686+SulamitaR@users.noreply.github.com>
2022-11-08 18:29:16 -0300

    Ajustando as imagens em quem somos

--- src/pages/QuemSomos/index.js
+++ src/pages/QuemSomos/index.js
@@ -5,22 +5,23 @@ const QuemSomos = () => {
         <center>
             <h1>Quem somos </h1>
 
-            <body>
-                <p>O Serratec – Parque Tecnológico da Região Serrana é uma instituição privada, sem fins lucrativos e de interesse público.</p>
-                <p> Principal indutor de negócios em TI do interior do estado do Rio de Janeiro, o
-                    Serratec é também o maior Parque Tecnológico do interior fluminense, abrangendo as cidades de Petrópolis, Teresópolis, Nova Friburgo e Areal.</p>
-                <p> E Nele tem o melhor site de compra/venda e para alugueis de imóveis da Região serrana</p>
 
+            <p>O Serratec – Parque Tecnológico da Região Serrana é uma instituição privada, sem fins lucrativos e de interesse público.</p>
+            <p> Principal indutor de negócios em TI do interior do estado do Rio de Janeiro, o
+                Serratec é também o maior Parque Tecnológico do interior fluminense, abrangendo as cidades de Petrópolis, Teresópolis, Nova Friburgo e Areal.</p>
+            <p> E Nele tem o melhor site de compra/venda e para alugueis de imóveis da Região serrana</p>
+
+            <div className="row">
                 {/* Sulamita Rodrigues Card */}
 
-                <div className="col-12 col-md-6 col-lg-4 card-3">
+                <div className=" col-sm-3 card-3">
                     <div className="card text-center">
                         <div className="card-body">
                             <img src="https://avatars.githubusercontent.com/u/111604686?v=4" alt="foto de perfil"
                                 className="img-fluid rounded-circle" />
                             <h3 className="card-title py-2" >Sulamita Rodrigues</h3>
                             <p className="card-text">
-                                - Corretora - 
+                                - Corretora -
                             </p>
 
                             <p className="card-text">
@@ -34,14 +35,14 @@ const QuemSomos = () => {
 
                 {/* Richard Cardinot Card */}
 
-                <div className="col-12 col-md-6 col-lg-4 card-3">
+                <div className="col-sm-3 card-3">
                     <div className="card text-center">
                         <div className="card-body">
                             <img src="https://avatars.githubusercontent.com/u/73504375?v=4" alt="foto de perfil"
                                 className="img-fluid rounded-circle" />
                             <h3 className="card-title py-2" >Richard Cardinot</h3>
                             <p className="card-text">
-                                - Corretor - 
+                                - Corretor -
                             </p>
                             <p className="card-text">
                                 Teresópolis - RJ
@@ -52,14 +53,14 @@ const QuemSomos = () => {
 
                 {/* Márcia Lima Card */}
 
-                <div className="col-12 col-md-6 col-lg-4 card-3">
+                <div className="col-sm-3 card-3">
                     <div className="card text-center">
                         <div className="card-body">
                             <img src="https://avatars.githubusercontent.com/u/100387127?v=4" alt="foto de perfil"
                                 className="img-fluid rounded-circle" />
                             <h3 className="card-title py-2" >Marcia Lima</h3>
                             <p className="card-text">
-                                - Corretora - 
+                                - Corretora -
                             </p>
                             <p className="card-text">
                                 Teresópolis - RJ
@@ -69,14 +70,14 @@ const QuemSomos = () => {
                 </div>
                 {/* Gabriel Pacheco Card */}
 
-                <div className="col-12 col-md-6 col-lg-4 card-3">
+                <div className="col-sm-3 card-3">
                     <div className="card text-center">
                         <div className="card-body">
                             <img src="https://avatars.githubusercontent.com/u/112281707?v=4" alt="foto de perfil"
                                 className="img-fluid rounded-circle" />
                             <h3 className="card-title py-2" >Gabriel Pacheco</h3>
                             <p className="card-text">
-                                - Corretor - 
+                                - Corretor -
                             </p>
                             <p className="card-text">
                                 Teresópolis - RJ
@@ -87,14 +88,14 @@ const QuemSomos = () => {
 
                 {/* Gabriel Mendes Card */}
 
-                <div className="col-12 col-md-6 col-lg-4 card-3">
+                <div className="col-sm-3 card-3">
                     <div className="card text-center">
                         <div className="card-body">
                             <img src="https://avatars.githubusercontent.com/u/98937422?v=4" alt="foto de perfil"
                                 className="img-fluid rounded-circle" />
                             <h3 className="card-title py-2" >Gabriel Mendes</h3>
                             <p className="card-text">
-                                - Corretor - 
+                                - Corretor -
                             </p>
                             <p className="card-text">
                                 Teresópolis - RJ
@@ -105,14 +106,14 @@ const QuemSomos = () => {
 
                 {/* Pedro Veiga Card */}
 
-                <div className="col-12 col-md-6 col-lg-4 card-3">
+                <div className="col-sm-3 card-3">
                     <div className="card text-center">
                         <div className="card-body">
                             <img src="https://avatars.githubusercontent.com/u/112408095?v=4" alt="foto de perfil"
                                 className="img-fluid rounded-circle" />
                             <h3 className="card-title py-2" >Pedro Veiga</h3>
                             <p className="card-text">
-                                - Corretor - 
+                                - Corretor -
                             </p>
                             <p className="card-text">
                                 Nova Friburgo - RJ
@@ -120,27 +121,22 @@ const QuemSomos = () => {
                         </div>
                     </div>
                 </div>
+            </div>
 
 
 
-
-                <div className="social">
-                    <a href="https://pt-br.facebook.com/serratecoficial/" target="_blank"><i
-                        className="bi bi-facebook"></i></a>
-                    <a href="https://www.instagram.com/serratecoficial/" target="_blank"><i
-                        className="bi bi-instagram"></i></a>
-                    <a href="https://www.linkedin.com/company/serratecoficial/" target="_blank"><i
-                        className="bi bi-linkedin"></i></a>
-                    <a href="https://www.youtube.com/channel/UC2pvxl7H3IK2X6z9XZ3xsyQ" target="_blank"><i
-                        className="bi bi-youtube"></i></a>
-                    <a href="https://github.com/Material-Didatico-Serratec" target="_blank"><i
-                        className="bi bi-github"></i></a>
-                </div>
-
-
-
-            </body>
-
+            <div className="social">
+                <a href="https://pt-br.facebook.com/serratecoficial/" target="_blank"><i
+                    className="bi bi-facebook"></i></a>
+                <a href="https://www.instagram.com/serratecoficial/" target="_blank"><i
+                    className="bi bi-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/serratecoficial/" target="_blank"><i
+                    className="bi bi-linkedin"></i></a>
+                <a href="https://www.youtube.com/channel/UC2pvxl7H3IK2X6z9XZ3xsyQ" target="_blank"><i
+                    className="bi bi-youtube"></i></a>
+                <a href="https://github.com/Material-Didatico-Serratec" target="_blank"><i
+                    className="bi bi-github"></i></a>
+            </div>
         </center >
     )
 }
